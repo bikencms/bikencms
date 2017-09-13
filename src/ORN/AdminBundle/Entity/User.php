d39ba09480e55d2f6df54c6d88983495d48f8c67
NeoHuy <quanghuy9490@gmail.com>
2017-09-13 14:37:14 +0700

    Setup FOSUserBundle

---
+++ src/ORN/AdminBundle/Entity/User.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace ORN\AdminBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * User
+ *
+ * @ORM\Table(name="user")
+ * @ORM\Entity(repositoryClass="ORN\AdminBundle\Repository\UserRepository")
+ */
+class User
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
+
